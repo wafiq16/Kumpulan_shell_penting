90aa33afaf6d43fc32d8a6a47b1e6d754e450583
wafiq16 <wafiqkamal16@gmail.com>
2020-12-07 05:38:36 +0700

    add vlc installer

--- cvop.sh
+++ cvop.sh
@@ -2,6 +2,7 @@ sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove
 # sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
 # sudo apt update
 sudo apt-get install  build-essential
+
 sudo apt-get install  cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
 sudo apt-get install  python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev  libdc1394-22-dev  libjasper-dev
--- opencv.sh
+++ opencv.sh
@@ -1,5 +1,9 @@
 sudo apt-get update && sudo apt-get upgrade
+
 sudo apt-get install build-essential
+
+sudo apt-get install guvcview
+
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
@@ -26,11 +30,16 @@ wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.3.
 
 unzip opencv_contrib.zip
 
+# cd <opencv_build_directory>
+# cmake -DOPENCV_EXTRA_MODULES_PATH=<opencv_contrib>/modules <opencv_source_directory>
+# make -j4
+
 cd ~/opencv-3.3.0/
 
 mkdir build
 
 cd build
+
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \ -D CMAKE_INSTALL_PREFIX=/usr/local \ -D INSTALL_PYTHON_EXAMPLES=ON \ -D INSTALL_C_EXAMPLES=OFF \ -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \ -D BUILD_EXAMPLES=ON ..
 
