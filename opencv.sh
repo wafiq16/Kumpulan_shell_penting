be99e1176abfe0bde43ecacf73114cdddb07aa0c
wafiq16 <wafiqkamal16@gmail.com>
2020-12-08 22:53:38 +0700

    solve bugs at opencv contrib install

--- opencv.sh
+++ opencv.sh
@@ -43,7 +43,9 @@ mkdir build
 cd build
 
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE \ -D CMAKE_INSTALL_PREFIX=/usr/local \ -D INSTALL_PYTHON_EXAMPLES=ON \ -D INSTALL_C_EXAMPLES=OFF \ -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules opencv-3.3.0 \ -D BUILD_EXAMPLES=ON ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE \ -D CMAKE_INSTALL_PREFIX=/usr/local \ -D INSTALL_PYTHON_EXAMPLES=ON \ -D INSTALL_C_EXAMPLES=OFF \ -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules ~/opencv-3.3.0 \ -D BUILD_EXAMPLES=ON ..
+
+cmake -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules ~/opencv-3.3.0
 
 make -j4
 
