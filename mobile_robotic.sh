8896d97a32a932023cfabdc7fc8c17967d9247b4
wafiq16 <wafiqkamal16@gmail.com>
2020-12-17 17:59:18 +0700

    add mrpt installer for all ubuntu version and update installer

--- mobile_robotic.sh
+++ mobile_robotic.sh
@@ -2,4 +2,12 @@ sudo apt-get update -y
 
 sudo apt-get update
 
-sudo apt-get install -y mrpt-apps
+# sudo apt-get install -y mrpt-apps
+
+sudo add-apt-repository ppa:joseluisblancoc/mrpt   # develop branch
+    #sudo add-apt-repository ppa:joseluisblancoc/mrpt-stable   # master (stable releases) branch
+    #sudo apt update # Only required for Ubuntu 16.04
+sudo apt-get update
+
+sudo apt install libmrpt-dev mrpt-apps
+
