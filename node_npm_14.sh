8e4bba4d4e9c579cbc2312165b6aac136cd481f5
wafiq16 <wafiqkamal16@gmail.com>
2021-04-15 04:57:46 +0700

    updating npm installer for stable release

--- node_npm_14.sh
+++ node_npm_14.sh
@@ -1,13 +1,17 @@
 sudo apt-get update
-sudo apt-get -y install curl dirmngr apt-transport-https lsb-release ca-certificates vim
+# sudo apt-get -y install curl dirmngr apt-transport-https lsb-release ca-certificates vim
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - #u can change the version from 14 to another version
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - #u can change the version from 14 to another version
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - #same with above
+# curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - #same with above
+sudo apt-get -y install libssl1.0-dev
+sudo apt-get -y install nodejs-dev
+sudo apt-get -y install node-gyp
+sudo apt-get -y install npm
 
-sudo apt-get -y install nodejs 
+# sudo apt-get -y install nodejs 
 
-sudo apt-get install gcc g++ make
+sudo apt-get install build-essential
 
 #check the version for sure nodejs has been installed
 node --version
