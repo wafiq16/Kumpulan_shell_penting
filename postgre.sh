4ecf6222f064a94ce0a94ff17701e23b4bfae49d
wafiq16 <wafiqkamal16@gmail.com>
2021-04-29 01:34:48 +0700

    add pgadmin4 installer for control postgree from desktop gui

--- postgre.sh
+++ postgre.sh
@@ -9,6 +9,33 @@
 apt show postgresql
 sudo apt install postgresql postgresql-contrib
 
+#
+# Setup the repository
+#
+
+# Install the public key for the repository (if not done previously):
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+#
+# Install pgAdmin
+#
+
+# Install for both desktop and web modes:
+sudo apt install pgadmin4
+
+# Install for desktop mode only:
+sudo apt install pgadmin4-desktop
+
+# Install for web mode only: 
+sudo apt install pgadmin4-web 
+
+# Configure the webserver, if you installed pgadmin4-web:
+sudo /usr/pgadmin4/bin/setup-web.sh
+
+
 # check connection to postgree-sql
 # sudo su - postgres
 # psql
