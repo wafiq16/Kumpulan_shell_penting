fbd57e2e569cf4098fe14a327bd674358fe4ecf8
wafiq16 <wafiqkamal16@gmail.com>
2021-04-29 01:33:48 +0700

    add several tools for training model

--- for_learning.sh
+++ for_learning.sh
@@ -2,6 +2,8 @@ sudo apt-get update -y
 
 sudo apt-get install python-pip
 
+sudo apt install protobuf-compiler
+
 sudo apt-get install python3-pip
 
 pip install tensorflow
@@ -9,6 +11,27 @@ pip install tensorflow
 pip install keras
 
 pip install imageAI
+
+pip install pillow
+pip install lxml
+pip install jupyter
+pip install matplotlib
+pip install skbuild
+pip install opencv-python
+
+pip3 install pillow
+pip3 install lxml
+pip3 install jupyter
+pip3 install matplotlib
+pip3 install skbuild
+pip3 install tf_slim
+sudo apt-get install python3-opencv
+
+pip install PyQt5
+pip install lxml
+pip3 install PyQt5
+pip3 install lxml
+# pyrcc5 -o resources.py resources.qrc
 
 pip3 install tensorflow
 
