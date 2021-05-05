d69eb995e8ec90b49b400e76ca92d0f2f196a261
wafiq16 <wafiqkamal16@gmail.com>
2021-05-05 22:10:42 +0700

    add figma installer ubuntu(debian based)

---
+++ figma.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:chrdevs/figma && sudo apt update && sudo apt install figma-linux -y
+
+# when no key at apt update, run this
+sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 70F3445E637983CC
