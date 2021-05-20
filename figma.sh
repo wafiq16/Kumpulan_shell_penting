254e68a69919b63eda859a125b521df1134f9504
wafiq16 <wafiqkamal16@gmail.com>
2021-05-20 16:41:40 +0700

    install normal instalation. before script is broken

--- figma.sh
+++ figma.sh
@@ -1,4 +1,13 @@
-sudo add-apt-repository ppa:chrdevs/figma && sudo apt update && sudo apt install figma-linux -y
+# sudo add-apt-repository ppa:chrdevs/figma && sudo apt update && sudo apt install figma-linux -y
 
-# when no key at apt update, run this
-sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 70F3445E637983CC
+# # when no key at apt update, run this
+# sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 70F3445E637983CC
+
+# Base on this
+# https://snapcraft.io/install/figma-linux/debian
+
+sudo apt update
+sudo apt install snapd
+sudo snap install core
+
+sudo snap install figma-linux
