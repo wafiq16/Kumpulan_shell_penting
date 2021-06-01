6931135a5c3477a533bd16784df26217940a8114
wafiq16 <wafiqkamal16@gmail.com>
2021-06-02 01:20:03 +0700

    add flutter installer

--- android.sh
+++ android.sh
@@ -8,4 +8,13 @@ sudo apt install openjdk-11-jdk
 # sudo add-apt-repository ppa:maarten-fonville/android-studio
 sudo snap install android-studio --classic
 
+sudo snap install flutter --classic
+
+flutter sdk-path
+
+sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev
+
+flutter config --enable-linux-desktop
+
+
 sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 -y
