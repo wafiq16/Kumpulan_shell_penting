b2639eb01e391deabca66e077dbb7afd4899d771
wafiq16 <wafiqkamal16@gmail.com>
2021-12-13 22:07:55 +0700

    repair npm installer for minimum node js version and npm

--- node_npm_14.sh
+++ node_npm_14.sh
@@ -4,15 +4,19 @@ sudo apt-get update
 # curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - #u can change the version from 14 to another version
 
 # curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - #same with above
-sudo apt-get -y install libssl1.0-dev
-sudo apt-get -y install nodejs-dev
-sudo apt-get -y install node-gyp
-sudo apt-get -y install npm
+#sudo apt-get -y install libssl1.0-dev
+#sudo apt-get -y install nodejs-dev
+#sudo apt-get -y install node-gyp
+#sudo apt-get -y install npm
 
 # sudo apt-get -y install nodejs 
 
-sudo apt-get install build-essential
+#sudo apt-get install build-essential
 
 #check the version for sure nodejs has been installed
+
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 node --version
 npm --version
