ccc0bb7a39f16a4dca15bc12e4d3e4642d494df3
wafiq16 <wafiqkamal16@gmail.com>
2022-09-25 03:27:46 +0700

    add chrome

---
+++ chrome.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+rm google-chrome-stable_current_amd64.deb
