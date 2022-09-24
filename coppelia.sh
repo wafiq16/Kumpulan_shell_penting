79d9abdb270db3b9b65ec4e26e280d8c107986d5
wafiq16 <wafiqkamal16@gmail.com>
2022-09-25 03:38:41 +0700

    add coppelia installer

---
+++ coppelia.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+wget https://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_4_0_rev0_Ubuntu20_04.tar.xz
+
+tar -xvzf /home/wafiq/CoppeliaSim_Edu_V4_4_0_rev0_Ubuntu20_04.tar.xz
+
+rm CoppeliaSim_Edu_V4_4_0_rev0_Ubuntu20_04.tar.xz
