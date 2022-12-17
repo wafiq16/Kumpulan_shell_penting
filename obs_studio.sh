48019e5bab7f63765416eff3c5a1eb148a87971e
wafiq16 <wafiqkamal16@gmail.com>
2022-12-18 03:47:00 +0700

    update newest shell

--- obs_studio.sh
+++ obs_studio.sh
@@ -1,5 +1,5 @@
-sudo apt install ffmpeg
-
-sudo add-apt-repository ppa:obsproject/obs-studio \n
+sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
-sudo apt install obs-studio -y 
+sudo apt install ffmpeg obs-studio
+
+
